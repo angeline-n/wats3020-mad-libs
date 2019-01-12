e5c58a5f5e1ff8f9aea2c036a5047a3a93df6625
angeline-n <nguyenangeli@seattleu.edu>
2019-01-11 19:38:35 -0800

    deleted previous code, created function to work with form

--- js/main.js
+++ js/main.js
@@ -1,114 +1,96 @@
 // WATS 3020 Mad Libs Assignment
 
-/* For this assignment you must fill in the code below so that it functions
-to populate the text of the "Mad Libs" style story in the HTML.
+function madLibs(){
+    let honorific = document.getElementById('honorific').value;
 
-In order to achieve this goal, you will need to write prompt() statements
-to collect input from the user. The input you collect will be used to
-generate the story. */
+    let authorName = document.getElementById('authorName').value;
 
-/*
-let honorific = prompt('Please enter an honorific title (e.g. Mister or Colonel).', 'default');
+    let adjective1 = document.getElementById('adjective1').value;
 
-let authorName = prompt('What is your full name?', 'default');
+    let vehicle = document.getElementById('vehicle').value;
 
-let adjective1 = prompt('Please enter an adjective (i.e. a word used to describe something).', 'default');
+    let vehiclePart = document.getElementById('vehiclePart').value;
 
-let vehicle = prompt('Please enter a vehicle (e.g. plane, boat, etc.).', 'default');
+    let parking = document.getElementById('parking').value;
 
-let vehiclePart = prompt('Please enter a part of the vehicle you just named.', 'default');
+    let precious = document.getElementById('precious').value;
 
-let parking = prompt('Please enter where someone would park the aforementioned vehicle.', 'default');
+    let noiseMaker = document.getElementById('noiseMaker').value;
 
-let precious = prompt('Please name a precious object.', 'default');
+    let crowdBehavior = document.getElementById('crowdBehavior').value;
 
-let noiseMaker = prompt('Please name something that makes noise.', 'default');
+    let adjective2 = document.getElementById('adjective2').value;
 
-let crowdBehavior = prompt('Please name something that people do together as a group.', 'default');
+    let adjective3 = document.getElementById('adjective3').value;
 
-let adjective2 = prompt('Please enter another adjective (i.e. a word used to describe something).', 'default');
+    let color = document.getElementById('color').value;
 
-let adjective3 = prompt('Please enter a third adjective (i.e. a word used to describe something).', 'default');
+    let familyMember = document.getElementById('familyMember').value;
 
-let color = prompt('Please name a color.', 'default');
+    let organ = document.getElementById('organ').value;
 
-let familyMember = prompt('Please name a family relationship (e.g. sister, father, etc.).', 'default');
+    let bodyPart1 = document.getElementById('bodyPart1').value;
 
-let organ = prompt('Please name an organ (e.g. heart, liver, spleen, etc.).', 'default');
+    let bodyPart2 = document.getElementById('bodyPart2').value;
 
-let bodyPart1 = prompt('Please enter the name of a body part.', 'default');
+    let bodyPart3 = document.getElementById('bodyPart3').value;
 
-let bodyPart2 = prompt('Please enter the name of another body part.', 'default');
+    let thing = document.getElementById('thing').value;
 
-let bodyPart3 = prompt('Please enter the name of a third body part.', 'default');
+    let thing2 = document.getElementById('thing2').value;
 
-let thing = prompt('Please name an object.', 'default');
+    let thing3 = document.getElementById('thing3').value;
 
-let thing2 = prompt('Please name an object you might give somebody you love.', 'default');
+    let adjective4 = document.getElementById('adjective4').value;
 
-let thing3 = prompt('Please name an object you might give somebody you like.', 'default');
+    let adjective5 = document.getElementById('adjective5').value;
 
-let adjective4 = prompt('Please enter a word used to describe the vehicle.', 'default');
+    let userNumber = parseInt(document.getElementById('userNumber').value);
 
-let adjective5 = prompt('Please enter another word to describe the vehicle.', 'default');
+    let number2 = userNumber * 2;
 
-/* Section Numbers
+    let number3 = userNumber + 7;
 
-The poem we are using has section numbers. Let's allow the user to
-generate their own. */
+    let titleText = `O ${honorific}! My ${honorific}! by <small>${authorName}</small>`;
 
-/*
-let userNumber = parseInt(prompt('Please enter your favorite number.', 0), 10);
+    document.write (`
+        
+        <h1>${titleText}</h1>
 
-let number2 = userNumber * 2;
+        <pre>
+        ${userNumber}
+        O ${honorific}! my ${honorific}! our ${adjective1} trip is done;
+        The ${vehicle} has weather'd every rack, the ${precious} we sought is won;
+        The ${parking} is near, the ${noiseMaker} I hear, the people all ${crowdBehavior},
+        While follow ${bodyPart1} the steady keel, the vessel ${adjective4} and ${adjective5}:
+        But O ${organ}! ${organ}! ${organ}!
+        O the bleeding drops of ${color},
+        Where on the ${vehiclePart} my ${honorific} lies,
+        Fallen ${adjective2} and ${adjective3}.
 
-let number3 = userNumber + 7;
-*/
+        ${number2}
+        O ${honorific}! my ${honorific}! rise up and hear the ${noiseMaker};
+        Rise up-for you the ${thing} is flung-for you the bugle trills;
+        For you ${thing2} and ribbon'd ${thing3}-for you the shores a-crowding;
+        For you they ${crowdBehavior}, the swaying mass, their eager faces turning;
+        Here ${honorific}! dear ${familyMember}!
+        This ${bodyPart2} beneath your ${bodyPart3};
+        It is some dream that on the ${vehiclePart},
+        You've fallen ${adjective2} and ${adjective3}.
 
-// DO NOT EDIT BELOW THIS LINE /////////////////////////////////////////
-//
-// The code below his line handles variable replacement into the HTML file.
-// Please do not edit this code unless you are attempting a stretch goal.
+        ${number3}
+        My ${honorific} does not answer, his lips are pale and still;
+        My ${familyMember} does not feel my arm, he has no pulse nor will;
+        The ${vehicle} is anchor'd safe and sound, its voyage closed and done;
+        From fearful trip, the victor ${vehicle}, comes in with ${precious} won;
+        Exult, O shores, and ring, O ${noiseMaker}!
+        But I, with mournful tread,
+        Walk the ${vehiclePart} my ${honorific} lies,
+        Fallen ${adjective2} and ${adjective3}.
+        </pre>
 
-/*
+        <hr>
+        <p>The original text used for this exercise is <a href="http://famouspoetsandpoems.com/poets/walt_whitman/poems/17466">"O Captain! My Captain" by Walt Whitman.</a></p>
+    `);
 
-let titleText = `O ${honorific}! My ${honorific}! by <small>${authorName}</small>`;
-let titleHeading = document.querySelector("#madlib-title");
-titleHeading.innerHTML = titleText;
-
-let storyText = `
-
-    ${userNumber}
-    O ${honorific}! my ${honorific}! our ${adjective1} trip is done;
-    The ${vehicle} has weather'd every rack, the ${precious} we sought is won;
-    The ${parking} is near, the ${noiseMaker} I hear, the people all ${crowdBehavior},
-    While follow ${bodyPart1} the steady keel, the vessel ${adjective4} and ${adjective5}:
-    But O ${organ}! ${organ}! ${organ}!
-    O the bleeding drops of ${color},
-    Where on the ${vehiclePart} my ${honorific} lies,
-    Fallen ${adjective2} and ${adjective3}.
-
-    ${number2}
-    O ${honorific}! my ${honorific}! rise up and hear the ${noiseMaker};
-    Rise up-for you the ${thing} is flung-for you the bugle trills;
-    For you ${thing2} and ribbon'd ${thing3}-for you the shores a-crowding;
-    For you they ${crowdBehavior}, the swaying mass, their eager faces turning;
-    Here ${honorific}! dear ${familyMember}!
-    This ${bodyPart2} beneath your ${bodyPart3};
-    It is some dream that on the ${vehiclePart},
-    You've fallen ${adjective2} and ${adjective3}.
-
-    ${number3}
-    My ${honorific} does not answer, his lips are pale and still;
-    My ${familyMember} does not feel my arm, he has no pulse nor will;
-    The ${vehicle} is anchor'd safe and sound, its voyage closed and done;
-    From fearful trip, the victor ${vehicle}, comes in with ${precious} won;
-    Exult, O shores, and ring, O ${noiseMaker}!
-    But I, with mournful tread,
-    Walk the ${vehiclePart} my ${honorific} lies,
-    Fallen ${adjective2} and ${adjective3}.
-`;
-let storyParagraph = document.querySelector("#madlib-text");
-storyParagraph.innerHTML = storyText;
-
-*/
+}
